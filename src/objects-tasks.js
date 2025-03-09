5a58f1b50f09cded758e520031554b5c6ba79c01
Veranika <veranika.bazhanok@gmail.com>
2025-03-09 16:05:49 +0100

    feat: add 5 and 6 solutions

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -105,8 +105,12 @@ function compareObjects(obj1, obj2) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  const objLength = Object.keys(obj).length;
+  if (objLength === 0) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -125,8 +129,8 @@ function isEmptyObject(/* obj */) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
